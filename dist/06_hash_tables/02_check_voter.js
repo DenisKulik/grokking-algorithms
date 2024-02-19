8a7ed4da286983b81c665f0815e69bcdedd1475a
Denis Kulik <kulikdenis1996x@gmail.com>
2024-02-19 19:38:10 +0700

    add checkVoter function example

---
+++ dist/06_hash_tables/02_check_voter.js
@@ -0,0 +1,14 @@
+"use strict";
+const voted = {};
+function check_voter(name) {
+    if (voted[name]) {
+        console.log('kick them out!');
+    }
+    else {
+        voted[name] = true;
+        console.log('let them vote!');
+    }
+}
+check_voter("tom");
+check_voter("mike");
+check_voter("mike");
---
+++ src/06_hash_tables/02_check_voter.ts
@@ -0,0 +1,18 @@
+const voted: HashTable<boolean> = {};
+
+/**
+ * Check if a voter has voted
+ * @param name - name of voter
+ */
+function check_voter(name: string): void {
+    if (voted[name]) {
+        console.log('kick them out!');
+    } else {
+        voted[name] = true;
+        console.log('let them vote!');
+    }
+}
+
+check_voter("tom"); // let them vote!
+check_voter("mike"); // let them vote!
+check_voter("mike"); // kick them out!
