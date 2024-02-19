343acc21c2602f157d39443ec810b875ef50c1d0
Denis Kulik <kulikdenis1996x@gmail.com>
2024-02-19 19:40:37 +0700

    refactor: code style

--- src/06_hash_tables/02_check_voter.ts
+++ src/06_hash_tables/02_check_voter.ts
@@ -6,13 +6,13 @@ const voted: HashTable<boolean> = {};
  */
 function check_voter(name: string): void {
     if (voted[name]) {
-        console.log('kick them out!');
+        console.log('kick them out!')
     } else {
         voted[name] = true;
-        console.log('let them vote!');
+        console.log('let them vote!')
     }
 }
 
-check_voter("tom"); // let them vote!
-check_voter("mike"); // let them vote!
-check_voter("mike"); // kick them out!
+check_voter("tom") // let them vote!
+check_voter("mike") // let them vote!
+check_voter("mike") // kick them out!
